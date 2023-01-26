4b9e7c9663c5d330b3289a145dc9b7ba2ddb9a65
ChiefValentine <78746274+ChiefValentine@users.noreply.github.com>
2023-01-26 10:30:57 +0100

    Init Backend
    Initialized backend

---
+++ Backend/src/main/java/com/theteapottroopers/farmwatch/FarmwatchApplication.java
@@ -0,0 +1,13 @@
+package com.theteapottroopers.farmwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmwatchApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/theteapottroopers/farmwatch/FarmwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theteapottroopers.farmwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
