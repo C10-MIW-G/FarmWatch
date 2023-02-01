523745d3d9d95f74deb435478c6a89efa2b1a3eb
Dave Thijs <d.thijs@proton.me>
2023-02-01 12:51:19 +0100

    Enabled Cors

--- Backend/src/main/java/com/theteapottroopers/farmwatch/security/config/SecurityConfiguration.java
+++ Backend/src/main/java/com/theteapottroopers/farmwatch/security/config/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package com.theteapottroopers.farmwatch.security.config;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.util.List;
 
 /**
  * @author Dave Thijs <d.thijs@st.hanze.nl>
@@ -29,11 +25,13 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
-                .permitAll()
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers("/animal/all").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
