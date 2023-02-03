8b4089c9d4fa2c9730edbf0878f66e38d9949df5
Dave Thijs <d.thijs@proton.me>
2023-02-03 13:47:05 +0100

    Added docs for security config files

--- Backend/src/main/java/com/theteapottroopers/farmwatch/security/config/ApplicationConfig.java
+++ Backend/src/main/java/com/theteapottroopers/farmwatch/security/config/ApplicationConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 /**
  * @author Dave Thijs <d.thijs@st.hanze.nl>
  * <p>
- *
+ * Configuration for the authentication
  */
 @Configuration
 @RequiredArgsConstructor
