f70c78ed54082b850810b7c726c7f8dee05b211f
mspbp <63860410+mspbp@users.noreply.github.com>
2023-02-10 12:09:06 +0100

    Refactor so that user uses 1 service

--- Frontend/FarmWatch/src/app/service/user.service.ts
+++ Frontend/FarmWatch/src/app/service/user.service.ts
@@ -5,13 +5,17 @@ import { User } from '../model/user';
 import { environment } from 'src/environments/environment';
 
 @Injectable({providedIn: 'root'})
-export class UserDetailService {
+export class UserService {
   private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
   public getUserDetail(id: number): Observable<User> {
     return this.http.get<User>(`${this.apiServerUrl}/user/${id}`)
+  }
+
+  public updateUser(user: User): Observable<User>{
+    return this.http.put<User>(`${this.apiServerUrl}/user/update`, user);
   }
 
   
