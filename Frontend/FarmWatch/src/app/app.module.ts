d50961b9e9b88b14ecb9a164cbc572405a371ae9
HamburgerSandwich <eelkemulder@gmail.com>
2023-02-13 12:03:07 +0100

    changed update animal and add animal forms

--- Frontend/FarmWatch/src/app/app.module.ts
+++ Frontend/FarmWatch/src/app/app.module.ts
@@ -19,7 +19,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserDetailComponent } from './component/user-detail/user-detail.component';
-
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CreateTicketComponent } from './component/create-ticket/create-ticket.component';
 import { MatSelectModule } from '@angular/material/select';
@@ -28,6 +27,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { UpdateUserComponent } from './component/update-user/update-user.component';
 import { ToastrModule } from 'ngx-toastr';
+import { MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+
 
 @NgModule({
   declarations: [
@@ -59,6 +61,8 @@ import { ToastrModule } from 'ngx-toastr';
     ReactiveFormsModule,
     MatIconModule,
     MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     ToastrModule.forRoot({
       positionClass: "toast-bottom-right",
       preventDuplicates: true,
