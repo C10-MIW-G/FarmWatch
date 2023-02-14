33b11ca4969c7edd3b03928bca8ea5fd4130628c
Dave Thijs <d.thijs@proton.me>
2023-02-14 18:18:16 +0100

    Added the functionality to add a new Ticket message

--- Frontend/FarmWatch/src/app/service/ticket-message.service.ts
+++ Frontend/FarmWatch/src/app/service/ticket-message.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Ticket } from '../model/ticket';
 import { TicketMessage } from '../model/ticket-message';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({providedIn: 'root'})
 export class TicketMessageService {
@@ -14,5 +18,8 @@ export class TicketMessageService {
   public getTicketMessage(id: number): Observable<TicketMessage> {
     return this.http.get<TicketMessage>(`${this.apiServerUrl}/ticket/message/${id}`)
   }
-  
+
+  public addTicketMessage(data: any): Observable<any>{
+    return this.http.post(`${this.apiServerUrl}/ticket/message`, data);
+  }
 }
