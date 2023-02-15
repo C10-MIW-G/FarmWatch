5c40124c93b27514a308a486706aebff658efee3
mspbp <63860410+mspbp@users.noreply.github.com>
2023-02-15 11:26:31 +0100

    deleted component and added functionality to userdetail

--- Frontend/FarmWatch/src/app/app.module.ts
+++ Frontend/FarmWatch/src/app/app.module.ts
@@ -18,7 +18,6 @@ import { RECAPTCHA_SETTINGS, RecaptchaFormsModule, RecaptchaModule, RecaptchaSet
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AdminUserDetailComponent } from './component/admin-user-detail/admin-user-detail.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CreateTicketComponent } from './component/create-ticket/create-ticket.component';
@@ -47,7 +46,6 @@ import { UserDetailComponent } from './component/user-detail/user-detail.compone
     ProfileComponent,
     AdminDashboardComponent,
     UpdateAnimalComponent,
-    AdminUserDetailComponent,
     CreateTicketComponent,
     AdminUpdateUserComponent,
     TicketDetailComponent,
