31fc7fe4e9b48c1fc97ac397705236aa9b7d0005
mspbp <63860410+mspbp@users.noreply.github.com>
2023-02-22 16:56:18 +0100

    added countByRole method in UserRepo

--- Backend/src/main/java/com/theteapottroopers/farmwatch/repository/UserRepository.java
+++ Backend/src/main/java/com/theteapottroopers/farmwatch/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.theteapottroopers.farmwatch.repository;
 
+import com.theteapottroopers.farmwatch.security.user.Role;
 import com.theteapottroopers.farmwatch.security.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findUserByUsername(String username);
+    long countByRole(Role role);
+
 }
