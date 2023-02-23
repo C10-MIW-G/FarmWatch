7d775ff02f66a8e41e7f3d4663edc68b7bd4d85d
mspbp <63860410+mspbp@users.noreply.github.com>
2023-02-23 11:41:12 +0100

    made LoginException

---
+++ Backend/src/main/java/com/theteapottroopers/farmwatch/exception/LoginException.java
@@ -0,0 +1,13 @@
+package com.theteapottroopers.farmwatch.exception;
+
+/**
+ * @Author: M.S. Pilat <pilat_m@msn.com>
+ * <p>
+ * dit programma doet iets
+ */
+
+public class LoginException extends RuntimeException{
+
+    public LoginException (String message) { super(message); }
+
+}
