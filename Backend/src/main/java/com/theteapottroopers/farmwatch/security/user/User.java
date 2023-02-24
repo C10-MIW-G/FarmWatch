0312cc3e0c454f6722d165ed68bcc8f29f3d6904
mspbp <63860410+mspbp@users.noreply.github.com>
2023-02-24 15:52:27 +0100

    added annotation so user variables or also checked

--- Backend/src/main/java/com/theteapottroopers/farmwatch/security/user/User.java
+++ Backend/src/main/java/com/theteapottroopers/farmwatch/security/user/User.java
@@ -2,6 +2,7 @@ package com.theteapottroopers.farmwatch.security.user;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -31,10 +32,13 @@ public class User implements UserDetails {
     private String firstname;
     private String lastname;
     @Email
+    @NotNull
     @Column(name="email", nullable = false, unique = true)
     private String email;
+    @NotNull
     @Column(name="username", nullable = false, unique = true)
     private String username;
+    @NotNull
     @Column(nullable = false)
     private String password;
     @Enumerated(EnumType.STRING)
