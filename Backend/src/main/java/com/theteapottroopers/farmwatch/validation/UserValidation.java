4b9cc03fa86809619b99200f53aefec2d13dc5ea
mspbp <63860410+mspbp@users.noreply.github.com>
2023-02-24 13:26:02 +0100

    added UserValidation so for better practice

---
+++ Backend/src/main/java/com/theteapottroopers/farmwatch/validation/UserValidation.java
@@ -0,0 +1,60 @@
+package com.theteapottroopers.farmwatch.validation;
+
+import com.theteapottroopers.farmwatch.dto.UserDto;
+import com.theteapottroopers.farmwatch.exception.InputHasDuplicateException;
+import com.theteapottroopers.farmwatch.exception.LastAdminDeletionException;
+import com.theteapottroopers.farmwatch.repository.UserRepository;
+import com.theteapottroopers.farmwatch.security.user.Role;
+import com.theteapottroopers.farmwatch.security.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: M.S. Pilat <pilat_m@msn.com>
+ * <p>
+ * dit programma doet iets
+ */
+
+@Service
+public class UserValidation {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserValidation(UserRepository userRepository) {this.userRepository = userRepository;}
+
+    public void databaseValidation(UserDto userDto, User user) {
+        lastAdmin(userDto,user);
+        uniqueUsername(userDto,user);
+        uniqueEmail(userDto,user);
+    }
+
+    private void lastAdmin(UserDto userDto, User user) {
+        if (userRepository.countByRole(Role.ROLE_ADMIN) == 1 &&
+                user.getRole().equals(Role.ROLE_ADMIN) &&
+                !userDto.getRole().equals(Role.ROLE_ADMIN)) {
+            throw new LastAdminDeletionException("This user is the last Admin");
+        }
+    }
+
+    private void uniqueUsername(UserDto userDto, User user) {
+        if (userRepository.findUserByUsername(userDto.getUsername()).isPresent() &&
+                !userDto.getUsername().equals(user.getUsername())) {
+            throw new InputHasDuplicateException("Username is already taken");
+        }
+    }
+
+    private void uniqueEmail(UserDto userDto, User user) {
+        if (userRepository.findUserByEmail(user.getEmail()).isPresent() &&
+                !userDto.getEmail().equals(user.getEmail())) {
+            throw new InputHasDuplicateException("Email is already taken");
+        }
+    }
+
+
+
+
+
+
+
+}
