ec38d12adb7476d417b729e4c2f3e809251ecea2
mspbp <63860410+mspbp@users.noreply.github.com>
2023-02-24 09:48:43 +0100

    added exceoptionhandler for dublicate entry

--- Backend/src/main/java/com/theteapottroopers/farmwatch/exception/ApplicationExceptionHandler.java
+++ Backend/src/main/java/com/theteapottroopers/farmwatch/exception/ApplicationExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.time.DateTimeException;
 
 /**
@@ -64,6 +65,11 @@ public class ApplicationExceptionHandler {
                 .body(exception.getMessage());
     }
 
+    @ExceptionHandler(value = {SQLIntegrityConstraintViolationException.class})
+    public ResponseEntity<Object> handleCustomException(SQLIntegrityConstraintViolationException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Username or email already exists. Please try again");
+    }
 
 
 
