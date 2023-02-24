506c6d765b0ed359cb245a6b33e6cd7551478e57
mspbp <63860410+mspbp@users.noreply.github.com>
2023-02-24 15:51:11 +0100

    added new exception

---
+++ Backend/src/main/java/com/theteapottroopers/farmwatch/exception/SomethingWentWrongException.java
@@ -0,0 +1,12 @@
+package com.theteapottroopers.farmwatch.exception;
+
+/**
+ * @Author: M.S. Pilat <pilat_m@msn.com>
+ * <p>
+ * dit programma doet iets
+ */
+
+public class SomethingWentWrongException extends RuntimeException{
+
+    public SomethingWentWrongException (String message) { super(message); }
+}
