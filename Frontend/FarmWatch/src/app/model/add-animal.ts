6b3c2f463b40196fb03e115f54e142772b852a40
ChiefValentine <78746274+ChiefValentine@users.noreply.github.com>
2023-03-06 15:27:32 +0100

    updated Animal Detail from class to interface

--- Frontend/FarmWatch/src/app/model/add-animal.ts
+++ Frontend/FarmWatch/src/app/model/add-animal.ts
@@ -1,21 +1,11 @@
-export class AnimalToAdd {
+export interface AnimalToAdd {
     id: number;
     name: string;
     commonName: string;
     species: string;
     description: string;
     dateOfBirth: string;
-    imageUrl: string;
-
-    constructor() {
-        this.id = 0,
-        this.name = '',
-        this.commonName = '',
-        this.species = '',
-        this.description = '',
-        this.dateOfBirth = '',
-        this.imageUrl = ''
-    }
+    imageFileName: string;
 }
 
 
