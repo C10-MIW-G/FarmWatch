3f17a68a6c370435f2094c3ba21c3c56e49be467
ChiefValentine <78746274+ChiefValentine@users.noreply.github.com>
2023-03-24 10:05:54 +0100

    refector typo

--- Backend/src/test/java/com/theteapottroopers/farmwatch/FarmwatchApplicationTests.java
+++ Backend/src/test/java/com/theteapottroopers/farmwatch/FarmwatchApplicationTests.java
@@ -46,7 +46,7 @@ class FarmwatchApplicationTests {
 	AnimalValidation animalValidation;
 
 	@Test
-	@DisplayName("Validate if test images size does not exceed max of 2mb")
+	@DisplayName("Validate if test image size does not exceed max of 2mb")
 	void imageFileSizeValidationTest(){
 
 		Path smallFilePath = Paths.get("src/test/resources/images/test_image_1mb.jpg");
